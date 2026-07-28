b16fdca941f87e45b7dabb1c936dab30e3229fc3
mandriva19 <kobidzeff@gmail.com>
2026-07-28 23:12:52 +0400

    chore: save sanity deploy appId in config

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: '1g7tc639',
     dataset: 'production'
   },
+  studioHost: 'inventari-admin',
   deployment: {
+    appId: 'aoq85g8yky2r3saaie21f3ip',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
